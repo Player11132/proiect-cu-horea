d650bb899834274d8cc2bed675f0f97d3dd9902a
Player11132 <minecraftsteve990@gmail.com>
2021-03-13 17:31:13 +0200

    turtles

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("Help me")
+import turtle
+
+wn = turtle.Screen()
+wn.title("Window")
+wn.bgcolor("green")
+wn.setup(height=600,width=600)
+tur = turtle.Turtle()
+tur.color("white")
+tur.pendown()
+tur.pensize(2)
+tur.speed(0)
+tur.fillcolor("yellow")
+tur.begin_fill()
+for i in range(20):
+    tur.circle(40)
+    tur.left(140)
+tur.end_fill()
+wn.mainloop()
